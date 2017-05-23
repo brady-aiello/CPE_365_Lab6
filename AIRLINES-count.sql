6226271b2e3a0c827e3af3eeea372888ad93e930
Brady Aiello <bbaiello@calpoly.edu>
2017-05-22 20:55:26 -0700

    (Almost) finished AIRLINES. Q4 is not possible given the current schema.
    Signed-off-by: Brady Aiello <bbaiello@calpoly.edu>

---
+++ AIRLINES-count.sql
@@ -0,0 +1,42 @@
+-- bbaiello Brady Aiello
+
+-- Q1: Find all airports with exactly 17 outgoing flights. Report airport code
+--     and the full name of the airport sorted in alphabetical order by the
+--     code.
+
+-- SELECT ap.Code, ap.Name
+--     FROM airports ap, flights f
+--     WHERE 
+--         f.Source = ap.Code
+--     GROUP BY ap.Code
+--         HAVING COUNT(*) = 17
+--     ORDER BY ap.Code;
+
+-- Q2: Find the number of airports from which airport ANP can be reached with
+--     exactly 1 transfer.  (make sure to exclude ANP itself from the count).
+--     Report just the number.
+
+-- SELECT COUNT(DISTINCT f1.Source)
+--     FROM flights f1, flights f2
+--     WHERE
+--         f2.Destination = 'ANP' AND f2.Source = f1.Destination
+--         AND f1.Source <> 'ANP' AND f1.Destination <> 'ANP';
+ 
+-- Q3: Find the number of airports from which airport ATE can be reached with
+--     AT MOST one transfer. (make sure to exclude ATE itself from the count)
+
+SELECT COUNT(DISTINCT f1.Source)
+    FROM flights f1, flights f2
+    WHERE
+        f2.Destination = 'ATE' 
+        AND (f2.Source = f1.Destination AND f1.Source <> 'ATE' 
+            AND f1.Destination <> 'ATE')
+        OR (f1.Destination = 'ATE')
+    ORDER BY f1.Source;
+
+-- Q4: For each airline report the total number of airports from which it has
+--     at least one outgoing flight. Report the full name of the airline and
+--     the number of airports computed. Report the results sorted by the 
+--     number of airports in descending order.
+
+
